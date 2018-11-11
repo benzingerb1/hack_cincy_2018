377f646d1a2ca0acada6391606e04de437d08f28
benzingerb1 <benzingerb1@gmail.com>
2018-11-11 08:30:39 -0500

    worked out urls for /metronome and home

--- metronome/home/urls.py
+++ metronome/home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('metronome/', views.metronome, name='metronome-app')
 ]
--- metronome/home/views.py
+++ metronome/home/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def metronome(request):
+    return render(request, 'metronome-app.html')
