73bb648397ee4cbc7a4e609610eed798b547e68c
benzingerb1 <benzingerb1@gmail.com>
2018-11-10 22:09:58 -0500

    django api app is responsible for its own urls now

--- metronome/api/urls.py
+++ metronome/api/urls.py
@@ -1,1 +1,9 @@
-from resources import EntryResource, ExerciseResource
+from django.urls import path
+from django.conf.urls import include
+from api.resources import ExerciseResource
+
+exercise_resource = ExerciseResource()
+
+urlpatterns = [
+    path('', include(exercise_resource.urls)),
+]
--- metronome/metronome/urls.py
+++ metronome/metronome/urls.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
-from django.contrib.auth import views as auth_views
-from api.resources import EntryResource, ExerciseResource
-
-entry_resource = EntryResource()
-exercise_resource = ExerciseResource()
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(exercise_resource.urls)),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('api/', include('api.urls')),
 ]
