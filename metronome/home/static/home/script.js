38d31cb201a91f39edb971dda2563be730d518b0
benzingerb1 <benzingerb1@gmail.com>
2018-11-11 10:06:33 -0500

    added 'next button' functionality to metronome app

--- metronome/home/static/home/script.js
+++ metronome/home/static/home/script.js
@@ -1,1 +1,21 @@
 console.log("hello world");
+
+$.ajax({
+    url: '/api/exercise',
+    dataType: 'json',
+    success: function (data) {
+        exercise = data.objects;
+        i = 2;
+        // initialize the title 
+        title = exercise[i].title
+        $("#exercise-title").text(title);
+        // assign "next title"
+        $("#test-button").click(function () {
+            i++;
+            title = exercise[i].title
+            $("#exercise-title").text(title);
+            console.log(title);
+        });
+
+    }
+})
