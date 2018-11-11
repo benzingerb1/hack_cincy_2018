813c6a66257a1fadb5e235626d063841e0cbd7f4
aswiehe <averyswiehe@gmail.com>
2018-11-10 22:19:46 -0500

    Adding python script to create titles

---
+++ create-scale-exercises.py
@@ -0,0 +1,13 @@
+# Create array holding notes A-G
+notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+# Create array holding all different scales (major, minor, pentatonic, myxolidian, dorian, etc)
+scales = ['Major', 'Minor', 'Major Pentatonic', 'Minor Pentatonic', 'Blues', 'Myxolidian', 'Dorian']
+
+# Outer for loop to go through each note
+for note in notes:
+    for scale in scales:
+        print(note, scale)
+# Inner for loop to go through each scale
+# Within each inner loop, print out the title of
+
+# YOU DO NOT NEED THE NOTES
